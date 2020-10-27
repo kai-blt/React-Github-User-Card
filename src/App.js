4f7e89a48df850314213002356f874abdf5924aa
kai-blt <blaitong@gmail.com>
2020-10-27 15:40:47 -0700

    added search functionality

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import Card from './components/Card'
 class App extends React.Component {
   constructor() {
     super();
+
+    const defaultUsers =  ['kai-blt', 'schroeder-g', 'ETBassist', 'kpace622'];
+
     this.state = {
-      user_names: ['kai-blt', 'schroeder-g', 'ETBassist', 'kpace622'],
+      search_value: '',
+      user_names: defaultUsers,
       user_data: [],
       followers_data: [],
       baseUrl: 'https://api.github.com/users/'
@@ -31,18 +35,59 @@ class App extends React.Component {
     })    
   }
 
+  //Get Search Data
+  getSearchData = () => {
+      Axios.all([Axios.get(`${this.state.baseUrl}${this.state.search_value}`), Axios.get(`${this.state.baseUrl}${this.state.search_value}/followers`)])
+      .then(Axios.spread((...res) => {
+        const userData = res[0].data;
+        const followersData = res[1].data;
+        // console.log(data);
+        this.setState({user_data: [...this.state.user_data, userData]})
+        this.setState({followers_data: [...this.state.followers_data, followersData]})
+        console.log(this.state)
+      }))
+      .catch(err => console.log(err.errors));
+    }    
+  
+
+
  
   //After component mounts, get User Data
   componentDidMount() {
     this.getUserData();
   }
 
-  
+
+  //After component updates, get User Data
+  componentDidUpdate() {
+    if (this.state.search_value === null) {
+      this.setState({user_names: this.defaultUsers})
+    } 
+  }
+
+  //Handlers
+  handleChange = (e) => {
+    this.setState({search_value: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.getSearchData()
+  }
+
+
   render() {
     return(
       <>
         <h1>GitHub User Fetch API</h1>
-        { this.state.user_data
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input type="text" value={this.state.search_value} onChange={this.handleChange}/>
+          </label>
+          <button>Search Users</button>
+        </form>
+
+        { this.state.search_value === ''
             ? this.state.user_data.map((user, index) => 
               <Card key={user.id}
                 login={user.login}
@@ -54,7 +99,16 @@ class App extends React.Component {
                 followersData={this.state.followers_data[index]}
               />
             )
-            : 'NO DATA' 
+            : this.state.user_data.filter(user => user.login === this.state.search_value).map((user, index) => 
+              <Card key={user.id}
+                login={user.login}
+                html_url={user.html_url}
+                avatar_url={user.avatar_url}   
+                location={user.location}
+                followers={user.followers}
+                following={user.following}
+                followersData={this.state.followers_data[index]}
+              />)
         }
       </>
     )
