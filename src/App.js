0e1105a8ff6b3fa166d440c2628f461497f11aaf
kai-blt <blaitong@gmail.com>
2020-10-27 08:50:42 -0700

    Added Axios request

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React from 'react'
+import Axios from 'axios'
+
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      user_names: ['kai-blt', 'ETBassist', 'kpace622'],
+      user_data: [],
+      baseUrl: 'https://api.github.com/users/'
+    }    
   }
 
+  
+
   componentDidMount() {
-    console.log('hi')
+    this.state.user_names.forEach(user => {
+      Axios.get(`${this.state.baseUrl}${user}`)
+      .then(res => {
+        const data = res.data;
+        console.log(data);
+        this.setState({user_data: data})
+      })
+      .catch(err => console.log(err.errors));
+    })        
   }
+
 
   render() {
     return(
-      <h1>Hello</h1>
+      <>
+        <h1>Hello</h1>
+        { this.state.user_data
+            ? 'I GOT DATA'
+            : 'NO DATA' 
+        }
+      </>
     )
   }
 }
